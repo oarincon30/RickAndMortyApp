cb506dd21cc72a04696037e3a8d6091760e42dc6
Oswaldo R. <oarincon30@ucatolica.edu.co>
2025-03-10 20:19:57 -0500

    Inicialización del proyecto Xcode

---
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Oswaldo Rincón on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyApp/RickAndMortyAppTests/RickAndMortyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppTests.swift
+//  RickAndMortyAppTests
+//
+//  Created by Oswaldo Rincón on 10/03/25.
+//
+
+import Testing
+@testable import RickAndMortyApp
+
+struct RickAndMortyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
